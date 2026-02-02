0050671a36566d0acd5a50f20a129747a3844b69
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2026-02-02 07:44:51 +0700

    update

--- Cau2.js
+++ Cau2.js
@@ -3,6 +3,7 @@ let countFizz = 0;
 for (let i = 1; i <= 50; i++) {
   if (i % 3 === 0) {
     console.log(`Các số chia hết cho 3 là: ${i} Fizz`);
+    if (i % 5 === 0) continue;
     countFizz += i;
   } else if (i % 5 === 0) {
     console.log(`Các số chia hết cho 5 là: ${i} Fizz`);
